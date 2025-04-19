02547d79965529961fbd4bdb215c8e4624cb566b
DylanJaveri <140866137+DylanJaveri@users.noreply.github.com>
2025-04-19 18:35:45 -0400

    Update BinarySearchTree_tests.cpp

--- BinarySearchTree_tests.cpp
+++ BinarySearchTree_tests.cpp
@@ -1,10 +1,57 @@
 #include "BinarySearchTree.hpp"
 #include "unit_test_framework.hpp"
+#include <sstream>
+
+using namespace std;
+TEST(bst_public_test) {
+  BinarySearchTree<int> tree;
+  BinarySearchTree<int> tree2(tree);
+  BinarySearchTree<int> tree4 = tree2;
+    ASSERT_TRUE(tree.empty());
+    ASSERT_TRUE(tree.check_sorting_invariant());
+    ASSERT_TRUE(tree.max_element() == tree.end());
+    ASSERT_TRUE(tree.min_element() == tree.end());
+    tree2.insert(100);
+    ASSERT_FALSE(tree2.find(100) == tree.end());
+    ASSERT_TRUE(tree.find(100) == tree.end());
+
+  tree.insert(5);
+BinarySearchTree<int> tree3(tree);
+BinarySearchTree<int> tree5 = tree;
+
+  ASSERT_FALSE(tree.empty());
+  ASSERT_TRUE(tree.size() == 1);
+  ASSERT_TRUE(tree.height() == 1);
+  ASSERT_TRUE(tree3.size() == 1);
+    ASSERT_TRUE(*tree3.min_element() == 5);
 
 
-TEST(test_stub) {
-    // Add your tests here
-    ASSERT_TRUE(true);
+  ASSERT_TRUE(tree.find(5) != tree.end());
+
+  tree.insert(7);
+  tree.insert(3);
+  ASSERT_TRUE(tree.check_sorting_invariant());
+  ASSERT_TRUE(*tree.max_element() == 7);
+  ASSERT_TRUE(*tree.min_element() == 3);
+  ASSERT_TRUE(*tree.min_greater_than(5) == 7);
+
+  cout << "cout << tree.to_string()" << endl;
+  cout << tree.to_string() << endl << endl;
+
+  cout << "cout << tree" << endl << "(uses iterators)" << endl;
+  cout << tree << endl << endl;
+
+  ostringstream oss_preorder;
+  tree.traverse_preorder(oss_preorder);
+  cout << "preorder" << endl;
+  cout << oss_preorder.str() << endl << endl;
+  ASSERT_TRUE(oss_preorder.str() == "5 3 7 ");
+
+  ostringstream oss_inorder;
+  tree.traverse_inorder(oss_inorder);
+  cout << "inorder" << endl;
+  cout << oss_inorder.str() << endl << endl;
+  ASSERT_TRUE(oss_inorder.str() == "3 5 7 ");
 }
 
 TEST_MAIN()
